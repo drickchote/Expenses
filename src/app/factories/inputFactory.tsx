7c916ee29202e737b772049f2cebbb671d111ab2
Marcos Adriano <mdrick19@gmail.com>
2023-01-26 23:05:18 -0300

    feat: add input factory

---
+++ src/app/factories/inputFactory.tsx
@@ -0,0 +1,19 @@
+import GenericInput from "../../components/inputs/GenericInput";
+import MoneyInput from "../../components/inputs/MoneyInput";
+import SelectInput from "../../components/inputs/SelectInput";
+import { InputProps } from "../../components/inputs/types";
+
+interface CardInputProps {
+  type: "text" | "money" | "select";
+}
+
+export const createInput = (props: CardInputProps & InputProps) => {
+  switch (props.type) {
+    case "text":
+      return <GenericInput {...props} />;
+    case "money":
+      return <MoneyInput {...props} />;
+    case "select":
+      return <SelectInput {...props} />;
+  }
+};
