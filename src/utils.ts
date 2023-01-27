6dfd70d4c30f8188b52a31db67df2c01bc387090
Marcos Adriano <mdrick19@gmail.com>
2023-01-27 12:06:28 -0300

    feat: add formatMoney function to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,8 @@
 export const MONEY_MASK = "R$"; // TODO put this in configuration screen
+
+export const formatMoney = (value: number): string => {
+  return value
+    .toFixed(2)
+    .replace(".", ",")
+    .replace(/(\d)(?=(\d{3})+,)/g, "$1.");
+};
