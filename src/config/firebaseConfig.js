c95929befcc9aa7dd1fdf7b741261f6108ca6e24
Marcos Adriano <mdrick19@gmail.com>
2023-02-04 19:51:50 -0300

    chore: add firebase config

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// import {...} from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const { firebaseConfig } = require("../config/config.json");
+
+// Initialize Firebase
+export const firestone = initializeApp(firebaseConfig);
