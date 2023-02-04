de0e995fc1de4e5f6922aba4aea4427c473cebda
Marcos Adriano <mdrick19@gmail.com>
2023-02-03 23:51:32 -0300

    feat: add moneyToFloat function to utils

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,23 @@ export const formatMoney = (value: number): string => {
     .replace(".", ",")
     .replace(/(\d)(?=(\d{3})+,)/g, "$1.");
 };
+
+/**
+ *
+ * @param value in format "R$ 0,00"
+ * @returns 0.00
+ */
+
+export const moneyToFloat = (value: string): number => {
+  let multiplyFactor = 1;
+
+  const decimals = value.split(",")[1];
+
+  if (decimals.length === 1) multiplyFactor = 0.1;
+
+  if (decimals.length === 3) multiplyFactor = 10;
+
+  const floatValue = parseFloat(value.replace(/[^\d\,]/, "").replace(",", "."));
+
+  return floatValue * multiplyFactor;
+};
