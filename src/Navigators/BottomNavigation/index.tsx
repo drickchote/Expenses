3740d45d8898948e8154e58cab76fb5195699d22
Marcos Adriano <mdrick19@gmail.com>
2024-10-07 17:13:43 -0300

    feat: add list icon

--- src/Navigators/BottomNavigation/index.tsx
+++ src/Navigators/BottomNavigation/index.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import AddExpense from "../../screens/AddExpense";
-import { AntDesign } from "@expo/vector-icons";
-import { useTheme } from "styled-components";
+import {  Feather } from "@expo/vector-icons";
 import { ExpenseList } from "../../screens/ExpenseList";
 
 const Tab = createBottomTabNavigator();
 
 const BottomNavigation = () => {
-  const theme = useTheme();
 
   return (
     <NavigationContainer>
@@ -22,9 +20,11 @@ const BottomNavigation = () => {
         <Tab.Screen
           name="ADICIONAR GASTO"
           component={AddExpense}
-          options={{ tabBarIcon: () => <AntDesign name="plus" size={24} /> }}
+          options={{ tabBarIcon: () => <Feather name="plus" size={24} /> }}
         />
-        <Tab.Screen name="Lista de gastos" component={ExpenseList} />
+        <Tab.Screen name="Lista de gastos" component={ExpenseList} 
+          options={{ tabBarIcon: () => <Feather name="list" size={24} /> }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
